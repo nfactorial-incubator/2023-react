6814d2d5d3368d711f907f2677feae0781bff1fc
nurymka <7456305+Nurymka@users.noreply.github.com>
2023-06-03 13:53:02 +0600

    chore: remove util fetcher fn

--- nfactorial-nextjs-starter/lib/utils.ts
+++ nfactorial-nextjs-starter/lib/utils.ts
@@ -2,32 +2,9 @@ import ms from "ms";
 
 export const timeAgo = (timestamp: Date, timeOnly?: boolean): string => {
   if (!timestamp) return "never";
-  return `${ms(Date.now() - new Date(timestamp).getTime())}${
-    timeOnly ? "" : " ago"
-  }`;
+  return `${ms(Date.now() - new Date(timestamp).getTime())}${timeOnly ? "" : " ago"
+    }`;
 };
-
-export async function fetcher<JSON = any>(
-  input: RequestInfo,
-  init?: RequestInit,
-): Promise<JSON> {
-  const res = await fetch(input, init);
-
-  if (!res.ok) {
-    const json = await res.json();
-    if (json.error) {
-      const error = new Error(json.error) as Error & {
-        status: number;
-      };
-      error.status = res.status;
-      throw error;
-    } else {
-      throw new Error("An unexpected error occurred");
-    }
-  }
-
-  return res.json();
-}
 
 export function nFormatter(num: number, digits?: number) {
   if (!num) return "0";
